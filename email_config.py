d00a7fc8dfe996fcfe671022ff540e03ef39def9
KRITIK SHIVANSHU <40329238+kritik123@users.noreply.github.com>
2020-03-14 20:57:43 +0530

    Create email_config.py

---
+++ email_config.py
@@ -0,0 +1,24 @@
+"""api keys and emails for sending the alert email""" 
+
+#private API key for Mailgun 
+MAILGUN_API_KEY = '0b1f04eee42d099d8e100a70862f10ac-9ce9335e-81d62f38' 
+
+#sandbox url for mailgun 
+SANDBOX_URL= 'sandbox879cd1469fc946e4a09c4d57fe23ac6d.mailgun.org' 
+
+#postmaster@sandbox_url 
+SENDER_EMAIL = 'test@sandbox879cd1469fc946e4a09c4d57fe23ac6d.mailgun.org' 
+
+#receiver email id 
+RECIPIENT_EMAIL = 'cartersedmund@gmail.com' 
+
+#Bolt Cloud accout API key 
+API_KEY = 'eed9c637-cf6c-4cad-849b-c46282df4946' 
+
+#Bolt device id 
+DEVICE_ID = 'BOLT11692220' 
+
+FRAME_SIZE = 10 
+
+MUL_FACTOR = 6
+
